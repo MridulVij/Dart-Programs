bed97c9233b0cefe9089d4059d6132f4e2a64afb
Mridul Vij <93833892+MridulVij@users.noreply.github.com>
2022-11-25 20:05:15 +0530

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,16 @@
+//import 'dart:ffi';
+//import 'dart:html_common';
+
+void variables() {
+  // 6 types of data types
+  int var1 = 10;
+  double var2 = 999999999999999;
+  String var3 = "Hello";
+  bool var4 = true;
+  //Lists var5 =
+  //maps var6 =
+}
+
+void main() {
+  variables();
+}
